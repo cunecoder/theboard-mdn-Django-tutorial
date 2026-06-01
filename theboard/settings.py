436c165ae50498a451a0893fd9ad7d32aae09879
David Marin <david.marin@student.cune.edu>
2026-06-01 16:43:02 -0700

    Change allowed host for render deployment

--- theboard/settings.py
+++ theboard/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['dmcunecoder.pythonanywhere.com', '127.0.0.1', 'localhost', '*']
+ALLOWED_HOSTS = ['dmcunecoder.pythonanywhere.com', '127.0.0.1', 'localhost', 'https://theboard-94xf.onrender.com']
 
 
 # Application definition
