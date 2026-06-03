9e34cd1dcd68290bf5d0aec39847c38e14bc7bfc
David Marin <david.marin@student.cune.edu>
2026-06-03 11:43:17 -0700

    Add custom domain to allowed host! Hehe :-)

--- theboard/settings.py
+++ theboard/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['dmcunecoder.pythonanywhere.com', '127.0.0.1', 'localhost', 'theboard-94xf.onrender.com']
+ALLOWED_HOSTS = ['dmcunecoder.pythonanywhere.com', '127.0.0.1', 'localhost', 'theboard-94xf.onrender.com', 'flypimar.com']
 
 
 # Application definition
